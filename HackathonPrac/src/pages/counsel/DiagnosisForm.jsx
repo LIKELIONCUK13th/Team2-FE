1bd6972aba54670c31606e47c811531ba68ff4af
seohyun <westhyun1107@gmail.com>
2025-07-17 01:58:31 +0900

    Orginized directory structure reupload(w/o NaviBar_sh)

--- HackathonPrac/src/pages/counsel/DiagnosisForm.jsx
+++ HackathonPrac/src/pages/counsel/DiagnosisForm.jsx
@@ -2,12 +2,10 @@ import React from "react";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "../../styles/counsel/DiagnosisForm.css";
-import NaviBar_sh from "../../components/NaviBar_sh"; // <NaviBar_sh /> 형태로 네비게이션 바 사용 가능
 
 export default function DiagnosisForm() { // jsx파일명 == functino명이어여 함, 파일명을 수정하는 게 아닌 이상 건들지 말 것!
   return (
     <div>
-        <NaviBar_sh />
         <h1>DiagnosisForm</h1>
     </div>
   );
--- HackathonPrac/src/pages/emobook/ShowBook.jsx
+++ HackathonPrac/src/pages/emobook/ShowBook.jsx
@@ -2,12 +2,10 @@ import React from "react";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "../../styles/emobook/ShowBook.css";
-import NaviBar_sh from "../../components/NaviBar_sh"; // <NaviBar_sh /> 형태로 네비게이션 바 사용 가능
 
 export default function ShowBook() { // jsx파일명 == functino명이어여 함, 파일명을 수정하는 게 아닌 이상 건들지 말 것!
   return (
     <div>
-        <NaviBar_sh />
         <h1>ShowBook</h1>
     </div>
   );
